1aa51530f64ae7909e7a7c05d02a56dc827ce8ae
myeongjoon_kim <myeongjoon_kim@tmax.co.kr>
2021-06-25 16:26:45 +0900

    ADD test model

--- src/main/java/com/portfoliomaker/controller/MainController.java
+++ src/main/java/com/portfoliomaker/controller/MainController.java
@@ -1,6 +1,7 @@
 package com.portfoliomaker.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,7 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MainController {
 
     @GetMapping()
-    public String main() {
+    public String main(Model model) {
+        model.addAttribute("hello", "hello");
         return "main/main";
     }
 }
