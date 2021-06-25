45b255b5fd473cc1da3cf7f3982f80ccc815cc28
myeongjoon_kim <myeongjoon_kim@tmax.co.kr>
2021-06-25 16:25:19 +0900

    ADD main view

---
+++ src/main/java/com/portfoliomaker/controller/MainController.java
@@ -0,0 +1,15 @@
+package com.portfoliomaker.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/")
+public class MainController {
+
+    @GetMapping()
+    public String main() {
+        return "main/main";
+    }
+}
