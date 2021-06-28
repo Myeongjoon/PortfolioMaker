2f5d9edd8f1f1bd6d4268519803839b214ae2531
myeongjoon_kim <myeongjoon_kim@tmax.co.kr>
2021-06-28 13:44:12 +0900

    ADD chrome driver

--- src/main/java/com/portfoliomaker/service/SeleniumService.java
+++ src/main/java/com/portfoliomaker/service/SeleniumService.java
@@ -3,20 +3,33 @@ package com.portfoliomaker.service;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+
+import java.util.Locale;
 
 @Service
 public class SeleniumService {
-    //Properties
+    Logger logger = LoggerFactory.getLogger(SeleniumService.class);
     public static final String WEB_DRIVER_ID = "webdriver.chrome.driver";
     public static final String WEB_DRIVER_PATH = "./src/main/resources/driver/91/chromedriver";
 
-    public void doProcess() {
+    public void setDriver() {
+        switch (System.getProperty("os.name").toLowerCase(Locale.ROOT)) {
+            case "windows 10":
+                System.setProperty(WEB_DRIVER_ID, WEB_DRIVER_PATH);
+                break;
+            default:
+                logger.info("unsupported type : " + System.getProperty("os.name"));
+        }
+    }
 
-        System.setProperty(WEB_DRIVER_ID, WEB_DRIVER_PATH);
+    public void doProcess() {
+        this.setDriver();
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.setCapability("ignoreProtectedModeSettings", true);
         WebDriver driver = new ChromeDriver(chromeOptions);
-        driver.get("https://www.google.com");
+        driver.get("https://securities.miraeasset.com/");
     }
 }
