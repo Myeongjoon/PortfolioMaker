7fb634ccfd08a70a446894d1c03cf401920b888f
myeongjoon_kim <myeongjoon_kim@tmax.co.kr>
2021-06-28 14:08:45 +0900

    ADD function

--- src/main/java/com/portfoliomaker/service/SeleniumService.java
+++ src/main/java/com/portfoliomaker/service/SeleniumService.java
@@ -1,8 +1,11 @@
 package com.portfoliomaker.service;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -12,6 +15,8 @@ import java.util.Locale;
 @Service
 public class SeleniumService {
     Logger logger = LoggerFactory.getLogger(SeleniumService.class);
+    protected WebDriverWait wait;
+    WebDriver driver;
     public static final String WEB_DRIVER_ID = "webdriver.chrome.driver";
     public static final String WEB_DRIVER_PATH_WINDOW = "src/main/resources/driver/91/window/chromedriver.exe";
 
@@ -23,13 +28,18 @@ public class SeleniumService {
             default:
                 logger.info("unsupported type : " + System.getProperty("os.name"));
         }
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.setCapability("ignoreProtectedModeSettings", true);
+        driver = new ChromeDriver(chromeOptions);
+        wait = new WebDriverWait(driver, 1000);
     }
 
     public void doProcess() {
         this.setDriver();
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.setCapability("ignoreProtectedModeSettings", true);
-        WebDriver driver = new ChromeDriver(chromeOptions);
-        driver.get("https://securities.miraeasset.com/");
+        driver.get("https://securities.miraeasset.com/main.do");
+        logger.info("encounter index.html");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("gnb")));
+        logger.info("found gnb");
+        //driver.findElement(By.partialLinkText("로그인")).click();
     }
 }
