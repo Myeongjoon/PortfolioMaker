910515911dbf80f60cc3fba1ad720ddde58a7c1b
myeongjoon_kim <myeongjoon_kim@tmax.co.kr>
2021-06-29 11:21:03 +0900

    ADD jquery

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -3,20 +3,27 @@ google.charts.setOnLoadCallback(drawChart);
 
 function drawChart() {
 
-  var data = google.visualization.arrayToDataTable([
-    ['Task', 'Hours per Day'],
-    ['Work', 11],
-    ['Eat', 2],
-    ['Commute', 2],
-    ['Watch TV', 2],
-    ['Sleep', 7]
-  ]);
+  $.ajax({
+    url: '/portfolio',
+    method: 'get',
+    context: this,
+    success: function (data, status, xhr) {
+      var data = google.visualization.arrayToDataTable([
+        ['Task', 'Hours per Day'],
+        ['Work', 11],
+        ['Eat', 2],
+        ['Commute', 2],
+        ['Watch TV', 2],
+        ['Sleep', 7]
+      ]);
 
-  var options = {
-    title: 'My Daily Activities'
-  };
+      var options = {
+        title: 'My Daily Activities'
+      };
 
-  var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+      var chart = new google.visualization.PieChart(document.getElementById('piechart'));
 
-  chart.draw(data, options);
+      chart.draw(data, options);
+    }
+  });
 }
