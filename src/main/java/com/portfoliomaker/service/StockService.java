ed9856df71254ea925b97a6d4eca16ba1900c98d
myeongjoon_kim <myeongjoon_kim@tmax.co.kr>
2021-07-05 13:34:43 +0900

    MODIFY error

--- src/main/java/com/portfoliomaker/service/StockService.java
+++ src/main/java/com/portfoliomaker/service/StockService.java
@@ -203,8 +203,10 @@ public class StockService {
     public void parseNaverStock() {
         List<StockMeta> list = stockMetaRepository.findAll();
         for (StockMeta stockMeta : list) {
-            naverParsingService.parseMeta(stockMeta);
-            stockMetaRepository.save(stockMeta);
+            if (stockMeta.location != null && stockMeta.location.equals("코스피")) {
+                naverParsingService.parseMeta(stockMeta);
+                stockMetaRepository.save(stockMeta);
+            }
         }
     }
 
