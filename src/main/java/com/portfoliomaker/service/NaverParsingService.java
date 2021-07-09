c2535dba16ea4f57f73b01de12f5fd8fb122c1b2
myeongjoon_kim <myeongjoon_kim@tmax.co.kr>
2021-07-09 09:27:48 +0900

    ADD todo

--- src/main/java/com/portfoliomaker/service/NaverParsingService.java
+++ src/main/java/com/portfoliomaker/service/NaverParsingService.java
@@ -54,6 +54,7 @@ public class NaverParsingService {
         Elements previousRate = document.select(".no_exday");
         previousRate.select(".sp_txt1").remove();
         String previousRateReplaced = previousRate.select("em").get(1).text().replace("%", "");
+        //TODO 여기서 에러날 경우 파일로 저장하도록 수정
         stockPrice.previousRate = StringUtil.parseDoubleMoney(previousRateReplaced);
         document.select("#time").select(".date").select("span").remove();
         String time = document.select("#time").select(".date").text();
